22970c8d8824126d1466ecb0f9092417c5776763
Dor Ben Lulu <benluludor@yahoo.com>
2020-01-12 14:31:52 +0200

    Done Desktop Design

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = pprocess.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 const bodyParser = require("body-parser");
 const path = require("path");
 const api = require("./server/routes/api.js");
